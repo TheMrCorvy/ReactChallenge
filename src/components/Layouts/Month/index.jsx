de15f83474e0f51870988a2fa314040d8af4d8c7
Gonzalo <gonzalosalvadorcorvalan@gmail.com>
2023-09-29 19:58:37 -0300

    monthly layout is officially responsive

--- src/components/Layouts/Month/index.jsx
+++ src/components/Layouts/Month/index.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import Grid from "@mui/material/Grid"
 import Button from "@mui/material/Button"
 import Alert from "@mui/material/Alert"
+import AlertTitle from "@mui/material/AlertTitle"
 import Stack from "@mui/material/Stack"
 
 import styles from "./Month.module.css"
@@ -81,53 +82,68 @@ function Month() {
 	}, [])
 
 	return (
-		<Grid container spacing={0} sx={{ height: "90vh" }}>
-			<Grid item xs sx={{ maxWidth: "12.5vw", border: "1px solid", background: "red" }}>
-				calendario chiquito
-			</Grid>
-			<Grid item xs className={styles.month_layout_column}>
-				<Grid container spacing={0}>
-					{dates &&
-						dates.map((date, i) => (
-							<Grid item xs className={styles.month_layout_cell} key={"monthly_layout_cell_" + i}>
-								<div>
-									{date.isToday ? (
-										<>
-											<Button
-												color="primary"
-												variant="contained"
-												size="small"
-												sx={{ marginBottom: 0 }}
-											>
-												{date.number}
-											</Button>
-										</>
-									) : (
-										<p>{date.number}</p>
-									)}
-								</div>
-								<Stack sx={{ width: "100%", mt: date.isToday ? "5px" : null }} spacing={0}>
-									<Alert icon={false} severity="error" className={styles.no_pading}>
-										<span style={{ padding: 0 }}>
-											texto de prueba para ver realmente que tan largo alcanza a ser el alert
-										</span>
-									</Alert>
-									<Alert icon={false} severity="error">
-										This is an error alert
-									</Alert>
-									<Alert icon={false} severity="error" className={styles.no_pading}>
-										<span style={{ padding: 0 }}>
-											texto de prueba para ver realmente que tan largo alcanza a ser el alert
-										</span>
-									</Alert>
-									<Alert icon={false} severity="error">
-										This is an error alert
-									</Alert>
-								</Stack>
-							</Grid>
-						))}
-				</Grid>
-			</Grid>
+		<Grid container spacing={0} className={styles.month_layout_column}>
+			{dates &&
+				dates.map((date, i) => (
+					<Grid item xs className={styles.month_layout_cell} key={"monthly_layout_cell_" + i}>
+						<div>
+							{date.isToday ? (
+								<>
+									<Button color="primary" variant="contained" size="small" sx={{ marginBottom: 0 }}>
+										{date.number}
+									</Button>
+								</>
+							) : (
+								<p>{date.number}</p>
+							)}
+						</div>
+						<Stack sx={{ width: "100%", mt: date.isToday ? "5px" : null }} spacing={0}>
+							<Alert
+								icon={false}
+								sx={{ display: { xs: "none", sm: "none", md: "block" } }}
+								severity="error"
+								className={styles.no_pading}
+							>
+								<AlertTitle>Error</AlertTitle>
+								<span style={{ padding: 0 }}>
+									texto de prueba para ver realmente que tan largo alcanza a ser el alert
+								</span>
+							</Alert>
+							<Alert
+								icon={false}
+								sx={{ display: { xs: "none", sm: "none", md: "block" } }}
+								severity="error"
+								className={styles.no_pading}
+							>
+								<AlertTitle>Error</AlertTitle>
+								<span style={{ padding: 0 }}>
+									texto de prueba para ver realmente que tan largo alcanza a ser el alert
+								</span>
+							</Alert>
+							<Alert
+								sx={{ display: { xs: "block", sm: "block", md: "none" }, px: 0.4, py: 0 }}
+								severity="info"
+								variant="filled"
+							>
+								{" "}
+							</Alert>
+							<Alert
+								sx={{ display: { xs: "block", sm: "block", md: "none" }, px: 0.4, py: 0 }}
+								severity="info"
+								variant="filled"
+							>
+								{" "}
+							</Alert>
+							<Alert
+								sx={{ display: { xs: "block", sm: "block", md: "none" }, px: 0.4 }}
+								severity="info"
+								variant="filled"
+							>
+								{" "}
+							</Alert>
+						</Stack>
+					</Grid>
+				))}
 		</Grid>
 	)
 }
