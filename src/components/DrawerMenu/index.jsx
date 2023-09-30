e02632c79e4db430d291ef7b788ca640ba528ae9
Gonzalo <gonzalosalvadorcorvalan@gmail.com>
2023-09-30 08:46:59 -0300

    finished mock form to add events

--- src/components/DrawerMenu/index.jsx
+++ src/components/DrawerMenu/index.jsx
@@ -3,10 +3,34 @@ import { useState, useContext, useEffect, useMemo } from "react"
 import Drawer from "@mui/material/Drawer"
 import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
+import InputLabel from "@mui/material/InputLabel"
+import MenuItem from "@mui/material/MenuItem"
+import FormControl from "@mui/material/FormControl"
+import Select from "@mui/material/Select"
+import TextField from "@mui/material/TextField"
+import Button from "@mui/material/Button"
+
+import SaveIcon from "@mui/icons-material/Save"
 
 import MiniCalendar from "../MiniCalendar"
 
 const DrawerMenu = ({ open, toggleDrawer }) => {
+	const [age, setAge] = useState("")
+
+	const handleChange = (event) => {
+		setAge(event.target.value)
+	}
+
+	const timeOptions = (interval) =>
+		Array.from(Array(24).keys()).reduce((time, hour) => {
+			Array.from(Array(60 / interval).keys()).map((i) => {
+				const timeItem = (+(hour + "." + i * interval)).toFixed(2).replace(".", ":")
+				time.push(timeItem)
+				return null
+			})
+			return time
+		}, [])
+
 	return (
 		<Drawer
 			anchor="left"
@@ -21,16 +45,62 @@ const DrawerMenu = ({ open, toggleDrawer }) => {
 				spacing={2}
 				sx={{
 					padding: "1rem",
+					maxWidth: "20rem",
 				}}
 			>
 				<Grid item xs={12}>
-					<Typography variant="h4" gutterBottom>
-						Add a new event
+					<Typography variant="h5" gutterBottom>
+						Add a new event:
 					</Typography>
 				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						id="event-title-input"
+						label="Event Title"
+						fullWidth
+						variant="filled"
+					/>
+				</Grid>
 
-				<Grid item xs={12} sx={{ width: "0rem" }}>
+				<Grid item xs={12}>
 					<MiniCalendar />
+				</Grid>
+				<Grid item xs={12}>
+					<FormControl fullWidth variant="filled">
+						<InputLabel id="time-select">Pick a Time</InputLabel>
+						<Select
+							labelId="time-select"
+							id="time-select-options"
+							value={age}
+							label="Age"
+							onChange={handleChange}
+							size="small"
+						>
+							{timeOptions(30).map((option, optionIndex) => (
+								<MenuItem
+									value={option}
+									key={`small-calendar-time-picker-${optionIndex}`}
+								>
+									{option} Hs
+								</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</Grid>
+				<Grid item xs={12}>
+					<TextField
+						id="filled-multiline-flexible"
+						label="Event Description"
+						multiline
+						rows={4}
+						variant="filled"
+						fullWidth
+					/>
+				</Grid>
+				<Grid item xs={12} sx={{ textAlign: "center" }}>
+					<Button variant="contained" disableElevation startIcon={<SaveIcon />}>
+						Save Event
+					</Button>
 				</Grid>
 			</Grid>
 		</Drawer>
--- src/components/MiniCalendar/index.jsx
+++ src/components/MiniCalendar/index.jsx
@@ -4,10 +4,6 @@ import Typography from "@mui/material/Typography"
 import Tooltip from "@mui/material/Tooltip"
 import IconButton from "@mui/material/IconButton"
 import Grid from "@mui/material/Grid"
-import InputLabel from "@mui/material/InputLabel"
-import MenuItem from "@mui/material/MenuItem"
-import FormControl from "@mui/material/FormControl"
-import Select from "@mui/material/Select"
 
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft"
 import ChevronRightIcon from "@mui/icons-material/ChevronRight"
@@ -19,7 +15,6 @@ import useDatesList from "../../hooks/useDatesList"
 
 const MiniCalendar = () => {
 	const [weeks, setWeeks] = useState([])
-	const [age, setAge] = useState("")
 
 	const { datesList, daysOfWeek } = useDatesList()
 	const theme = useTheme()
@@ -38,65 +33,29 @@ const MiniCalendar = () => {
 		setWeeks(weeksArr)
 	}
 
-	const handleChange = (event) => {
-		setAge(event.target.value)
-	}
-
-	const timeOptions = (interval) =>
-		Array.from(Array(24).keys()).reduce((time, hour) => {
-			Array.from(Array(60 / interval).keys()).map((i) => {
-				const timeItem = (+(hour + "." + i * interval)).toFixed(2).replace(".", ":")
-				time.push(timeItem)
-				return null
-			})
-			return time
-		}, [])
-
-	const miniDay = (day) => {
-		return (
-			<Typography
-				sx={{
-					background: day.isToday ? lightBlue[700] : theme.palette.background.paper,
-					textAlign: "center",
-					fontSize: 12,
-					lineHeight: "26px",
-					padding: theme.spacing(0.2),
-					borderStyle: "solid",
-					textTransform: "capitalize",
-					borderRadius: "50%",
-					color: day.isToday ? "white" : "inherit",
-					borderColor: theme.palette.background.paper,
-					height: 35,
-					width: 35,
-					"&:hover": {
-						backgroundColor: day.isToday ? lightBlue[800] : theme.palette.grey[100],
-						cursor: "pointer",
-					},
-				}}
-			>
-				{day.number}
-			</Typography>
-		)
-	}
-
-	const miniOption = () => (
-		<FormControl fullWidth variant="standard" sx={{ mt: 3 }}>
-			<InputLabel id="time-select">Hour</InputLabel>
-			<Select
-				labelId="time-select"
-				id="time-select-options"
-				value={age}
-				label="Age"
-				onChange={handleChange}
-				size="small"
-			>
-				{timeOptions(30).map((option, optionIndex) => (
-					<MenuItem value={option} key={`small-calendar-time-picker-${optionIndex}`}>
-						{option} Hs
-					</MenuItem>
-				))}
-			</Select>
-		</FormControl>
+	const miniDay = (day) => (
+		<Typography
+			sx={{
+				background: day.isToday ? lightBlue[700] : theme.palette.background.paper,
+				textAlign: "center",
+				fontSize: 12,
+				lineHeight: "26px",
+				padding: theme.spacing(0.2),
+				borderStyle: "solid",
+				textTransform: "capitalize",
+				borderRadius: "50%",
+				color: day.isToday ? "white" : "inherit",
+				borderColor: theme.palette.background.paper,
+				height: 35,
+				width: 35,
+				"&:hover": {
+					backgroundColor: day.isToday ? lightBlue[800] : theme.palette.grey[100],
+					cursor: "pointer",
+				},
+			}}
+		>
+			{day.number}
+		</Typography>
 	)
 
 	return (
@@ -135,9 +94,6 @@ const MiniCalendar = () => {
 					</Grid>
 				</Grid>
 			))}
-			<Grid item xs={12} sx={{ mb: 3 }}>
-				{miniOption()}
-			</Grid>
 		</Grid>
 	)
 }
